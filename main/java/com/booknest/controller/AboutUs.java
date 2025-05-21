47b541e3c54099c2110e90a759d7c8f9ea372efd
noble nepal <nepalnoble525@gmail.com>
2025-05-21 12:33:36 +0545

    Merge Conflict Fixed in About us

--- main/java/com/booknest/controller/AboutUs.java
+++ main/java/com/booknest/controller/AboutUs.java
@@ -18,28 +18,6 @@ import java.io.IOException;
 public class AboutUs extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/pages/about-us.jsp").forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-}
-
-    private static final long serialVersionUID = 1L;
     
     // Path constant
     private final String aboutUsPagePath = "/WEB-INF/pages/about-us.jsp";
